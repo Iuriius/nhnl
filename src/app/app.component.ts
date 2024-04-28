9fe75fccea57b784996b353c8e0945923a87c6be
Iurii Kyrychenko <105295809+Iuriius@users.noreply.github.com>
2024-04-28 23:52:23 +0300

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +11,31 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'nhnl';
+  i = 0;
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.platformId = platformId;
+    if (isPlatformBrowser(platformId)) {
+      // runs on client / browser
+      console.log("Output is generated only in browser not in server.");
+    }
+    if (isPlatformServer(platformId)) {
+      // runs on server / node
+      console.log("Output is generated only in server not in browser.");
+    }
+  }
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      // runs on client / browser
+      console.log("Output is generated only in browser not in server.");
+    }
+    if (isPlatformServer(this.platformId)) {
+      // runs on server / node
+      console.log("Output is generated only in server not in browser.");
+    }
+  }
+
+  buttonClick() {
+    this.i++;
+  }
 }
