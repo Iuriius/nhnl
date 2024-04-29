4e9cc43acec7d94018d8bf8e9bbbfd1fe9bfbf23
Iurii Kyrychenko <105295809+Iuriius@users.noreply.github.com>
2024-04-29 15:13:29 +0300

    1

--- src/app/components/heroswiper/heroswiper.component.ts
+++ src/app/components/heroswiper/heroswiper.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-heroswiper',
   standalone: true,
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './heroswiper.component.html',
   styleUrl: './heroswiper.component.scss'
 })
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeroswiperComponent } from '../heroswiper/heroswiper.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeroswiperComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
