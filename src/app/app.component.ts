5e257547bb52aa2cf16370d6e1c4467a4dacd57b
Iurii Kyrychenko <105295809+Iuriius@users.noreply.github.com>
2024-04-29 10:21:44 +0300

    222222

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
-import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { CommonModule, isPlatformBrowser, isPlatformServer } from '@angular/common';
 import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'nhnl';
-  i = 0;
-  bool = false;
+
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {
     this.platformId = platformId;
     if (isPlatformBrowser(platformId)) {
@@ -34,15 +34,5 @@ export class AppComponent {
       // runs on server / node
       // console.log("Output is generated only in server not in browser.");
     }
-  }
-
-  buttonClick() {
-    this.i++;
-    console.log("clicked");
-  }
-
-  toggle() {
-    this.bool = !this.bool;
-    console.log("clicked");
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '', pathMatch: 'full' },
+    { path: '**', component: NotfoundComponent },
+];
